d23a71d6b2b4ebed24fe49ce572a115a0116f3f2
Nick Pittman <np0035@uah.edu>
2022-07-16 11:54:17 -0500

    Added comments to main script

--- main.m
+++ main.m
@@ -18,14 +18,18 @@ tic();
 % Begin simulation
 positions = driver(planets, G, precision, steps);
 
+% Display stats
 fprintf('%d planet trajectories extrapolated over %d timesteps.\n', ...
     length(planets), steps);
 fprintf('Elapsed time: %f sec\n', toc());
 
+% Transform position data into plottable matrices
 x = squeeze(positions(1,:,:)).';
 y = squeeze(positions(2,:,:)).';
 
+% Export CSV files
 writematrix(x,'x-positions.csv');
 writematrix(y,'y-positions.csv');
 
+% Plot data
 plot(x,y)
