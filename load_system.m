f9bcb92596f90c23c4e2a971972b8ba33ccefeab
Nick Pittman <np0035@uah.edu>
2022-07-16 18:34:25 -0500

    Function now loads system based on user choice

--- load_system.m
+++ load_system.m
@@ -5,15 +5,34 @@ function planets = load_system()
     % planets - An array of planet structures
 
     %% Display the current list of systems
-    fprintf('Systems available to load:\n');
-
-    % Get list of files
-    files = dir('saved_systems');
-    system_names = files;
-
-    % Separate them into just the names of the files and display them
-    for i = 1:length(system_names)
-        [~,system,~] = fileparts(system_names(i).name);
-        disp(system);
+    while (1)
+        fprintf('Systems available to load:\n');
+    
+        % Get list of files
+        files = dir('saved_systems');
+        system_names = files;
+    
+        % Separate them into just the names of the files and display them
+        for i = 1:length(system_names)
+            [~,system,~] = fileparts(system_names(i).name);
+            disp(system);
+        end
+    
+        %% Allow the user to choose a system
+        user_choice = input(['Type the name of ' ...
+            'one of the systems to load it, or type 0 to cancel: '],"s");
+        planets = [];
+    
+        try
+            planets = load(fullfile('saved_systems',user_choice));
+            fprintf('%s loaded\n', user_choice)
+            return
+        catch
+            if strcmp('0',user_choice)
+                planets = [];
+                return
+            end
+            fprintf('\nInvalid system; please try again.')
+        end
     end
 end
