489e1436867f18e80292d6bdd58f8648116de653
Nick Pittman <np0035@uah.edu>
2022-07-16 18:20:31 -0500

    Function now displays list of systems

--- load_system.m
+++ load_system.m
@@ -1,7 +1,19 @@
-function [outputArg1,outputArg2] = load_system(inputArg1,inputArg2)
-%LOAD_SYSTEM Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+function planets = load_system()
+    % This function loads a system saved to the hard drive.
+    %
+    % Outputs:
+    % planets - An array of planet structures
 
+    %% Display the current list of systems
+    fprintf('Systems available to load:\n');
+
+    % Get list of files
+    files = dir('saved_systems');
+    system_names = files;
+
+    % Separate them into just the names of the files and display them
+    for i = 1:length(system_names)
+        [~,system,~] = fileparts(system_names(i).name);
+        disp(system);
+    end
+end
