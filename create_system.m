a0b5936ff0a38bdca8277e1b5dd4ed9837c146e3
Nick Pittman <np0035@uah.edu>
2022-07-16 17:00:17 -0500

    Added user instructions and parameter prompts

--- create_system.m
+++ create_system.m
@@ -3,7 +3,38 @@ function planets = create_system()
     %
     % Outputs:
     % planets - An array of planet structures
-    
-    input('')
+
+    name = "";
+    num_planets = 0;
+    planet_max = 10;
+
+    % User instructions
+    fprintf(['\nEnter data for each planet. Type "/" for the planet name' ...
+        'to stop. %d planet maximum.'], planet_max);
+    fprintf(['\nInput positions and velocity values as a 2D vector in ' ...
+        'the format [x,y]']);
+
+    % Not an infinite loop - planet cap is in place
+    while (1)
+
+        % Prompt for name and test for exit value
+        name_prompt = sprintf('\nPlanet %d name: ', num_planets+1);
+        name = input(name_prompt,"s");
+        if (strcmp(name,"\"))
+            break
+        end
+        
+        % Prompt for other vars
+        pos = input('\nInitial position: ');
+        vel = input('\nInitial velocity: ');
+        mass = input('\nPlanet mass: ')
+
+        num_planets = num_planets + 1;
+
+        % Exit if planet cap is exceeded
+        if (num_planets > 10)
+            break
+        end
+    end
 end
 
