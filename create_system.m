e71fba8d525b9a7c95345d8bbfc6f5363afb4b15
Nick Pittman <np0035@uah.edu>
2022-07-16 16:41:51 -0500

    Add create_system function file

---
+++ create_system.m
@@ -0,0 +1,9 @@
+function planets = create_system()
+    % This function allows the user to create a new system of planets.
+    %
+    % Outputs:
+    % planets - An array of planet structures
+    
+    input('')
+end
+
