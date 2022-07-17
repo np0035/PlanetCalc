d16e6ab933628b3784f8ffda660324c8684ae72a
Nick Pittman <np0035@uah.edu>
2022-07-17 13:45:40 -0500

    Function displays current system and prompts user for a planet to edit

--- edit_system.m
+++ edit_system.m
@@ -1,4 +1,13 @@
 function planets = edit_system(pls)
-    % This function allows the user to edit a system.
-end
+    fprintf('Current system parameters:\n\n');
+    for i = 1:length(pls)
+        fprintf('%s\n-------\n', pls(i).name);
+        fprintf('Position: [%s]\n', num2str(pls(i).pos));
+        fprintf('Velocity: [%s]\n', num2str(pls(i).vel));
+        fprintf('Mass: %f\n\n', pls(i).mass);
+    end
 
+    edit_name = input('Type the name of the planet to edit: ');
+    
+    
+end
