7f148f80dadebf2ce7f3ebe32e8c005fec8b42ac
Nick Pittman <np0035@uah.edu>
2022-07-17 13:57:20 -0500

    Function gets planet to edit from user

--- edit_system.m
+++ edit_system.m
@@ -1,13 +1,30 @@
 function planets = edit_system(pls)
-    fprintf('Current system parameters:\n\n');
-    for i = 1:length(pls)
-        fprintf('%s\n-------\n', pls(i).name);
-        fprintf('Position: [%s]\n', num2str(pls(i).pos));
-        fprintf('Velocity: [%s]\n', num2str(pls(i).vel));
-        fprintf('Mass: %f\n\n', pls(i).mass);
-    end
+    while(1)
 
-    edit_name = input('Type the name of the planet to edit: ');
+        % Display current system
+        fprintf('Current system parameters:\n\n');
+        for i = 1:length(pls)
+            fprintf('%s\n-------\n', pls(i).name);
+            fprintf('Position: [%s]\n', num2str(pls(i).pos));
+            fprintf('Velocity: [%s]\n', num2str(pls(i).vel));
+            fprintf('Mass: %f\n\n', pls(i).mass);
+        end
     
-    
+        % Prompt user for planet to edit
+        edit_name = input(['Type the name of the planet to edit, or type ' ...
+            '"0" to save: '],"s");
+
+        % Stop editing if user types 0
+        if strcmp(edit_name, '0')
+            break
+        end
+
+        % Get planet to edit
+        for i = 1:length(pls)
+            if strcmp(pls(i).name, edit_name)
+                edit_planet = pls(i);
+                break
+            end
+        end
+    end
 end
