3752003ebfc9da5d67f5cba613ca618369c662e8
Nick Pittman <np0035@uah.edu>
2022-07-17 17:51:12 -0500

    Implemented edit_planet() in edit_system() and improved feedback

--- edit_system.m
+++ edit_system.m
@@ -12,19 +12,27 @@ function planets = edit_system(pls)
     
         % Prompt user for planet to edit
         edit_name = input(['Type the name of the planet to edit, or type ' ...
-            '"0" to save: '],"s");
+            '"0" to save the new system: '],"s");
 
         % Stop editing if user types 0
         if strcmp(edit_name, '0')
             break
         end
 
-        % Get planet to edit
+        % Edit planet if name is found
+        is_planet_found = false;
         for i = 1:length(pls)
             if strcmp(pls(i).name, edit_name)
-                edit_planet = pls(i);
+                pls(i) = edit_planet(pls(i));
+                fprintf('%s edited.\n', pls(i).name);
+                is_planet_found = true;
                 break
             end
+        end
+
+        % Handle planet not found
+        if (~is_planet_found)
+            fprintf('%s not found in the current system.', edit_name);
         end
     end
 end
