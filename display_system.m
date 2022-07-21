95369664b0bd0fcd81b448a61d58e4718603032b
Nick Pittman <np0035@uah.edu>
2022-07-20 19:16:57 -0500

    Wrote function

--- display_system.m
+++ display_system.m
@@ -1,4 +1,23 @@
 function display_system(pls)
+    fprintf('Current system parameters:\n\n');
+        for i = 1:length(pls)
+            [pol_pos(1), pol_pos(2)] = cart2pol(pls(i).pos(1), ...
+                pls(i).pos(2));
 
+            [pol_vel(1), pol_vel(2)] = cart2pol(pls(i).vel(1), ...
+                pls(i).vel(2));
+
+            fprintf('%s\n-------\n', pls(i).name);
+
+            fprintf('Position: [%s]\n', num2str(pls(i).pos));
+            fprintf('    Cartesian: [%s]\n', num2str(pls(i).pos));
+            fprintf('    Polar: [%s]\n', num2str(pol_pos));
+
+            fprintf('Velocity: [%s]\n', num2str(pls(i).vel));
+            fprintf('    Cartesian: [%s]\n', num2str(pls(i).vel));
+            fprintf('    Polar: [%s]\n', num2str(pol_vel));
+
+            fprintf('Mass: %f\n\n', pls(i).mass);
+        end
 end
 
