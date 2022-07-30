239bbecf087fbe5113be92b6b5779c9fb3e72b43
Nick Pittman <np0035@uah.edu>
2022-07-30 11:59:32 -0500

    Encapsulated updates to forces in the system in a new function

--- step.m
+++ step.m
@@ -1,22 +1,6 @@
 function planets = step(universe, planets)
-    for p = 1:length(planets)
-        % Reset force vector
-        planets(p).current_force = [0,0];
-    end
-
-    for p1 = 1:length(planets)
-        % Add up all the gravities acting on p1 by the other planets
-        for p2 = 1:length(planets)
-
-            % We don't need to consider the planet itself
-            if ~compare_planets(planets(p1),planets(p2))
-
-                % Get force between planets
-                planets(p1).current_force = planets(p1).current_force ...
-                    + get_force(universe, planets(p1), planets(p2));
-            end
-        end
-    end
+    
+    planets = update_system_forces(universe, planets);
 
     for p = 1:length(planets)
         planets(p).acc = planets(p).current_force ./ planets(p).mass;
